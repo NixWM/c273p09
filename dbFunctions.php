9deea60a2194b5de4cbd22f2309c16081e62bdcc
Nix WM <16046530@myrp.edu.sg>
2021-01-12 12:20:39 +0800

    Change to server
    This reverts commit 9e04e85b6faf8ef78320a9aeb2eef7c679880396.

--- dbFunctions.php
+++ dbFunctions.php
@@ -8,9 +8,9 @@
 $host = "localhost";
 $link = mysqli_connect($host,$username,$password,$db) or 
         die(mysqli_connect_error());
- *
+ 
 
- */
+*/
 $connectstr_dbhost = '';
 $connectstr_dbname = '';
 $connectstr_dbusername = '';
@@ -35,6 +35,6 @@ if (!$link) {
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
-
+ 
 ?>
 
--- js/showWorldPopulation.js
+++ js/showWorldPopulation.js
@@ -1,19 +1,17 @@
-
 $(document).ready(function () {
+    var data = [];
+    var labels = [];
     $.ajax({
         type: "GET",
-        url: "http://localhost/C273/P09/getStatistics.php",
+        url: "getStatistics.php",
         cache: false,
         dataType: "JSON",
         success: function (response) {
 
-            var data = [];
-            var labels = [];
             for (i = 0; i < response.length; i++) {
-                data.push(parseInt(response[i].population));
+                data.push(response[i].population);
                 labels.push(response[i].country);
             }
-
             var barChart = new Chart($("#barChart"), {
                 type: 'horizontalBar',
                 data: {
@@ -28,6 +26,9 @@ $(document).ready(function () {
                     responsive: true
                 }
             });
+        },
+        error: function (obj, textStatus, errorThrown) {
+            console.log("Error " + textStatus + ": " + errorThrown);
         }
     });
 });
